bbcc998a8000ab8bc2cae31bdd177831c7f2e894
Skyler Shuman <42816999+skyler-shuman@users.noreply.github.com>
2022-07-15 13:59:29 -0400

    Working with REST

--- practice/main.go
+++ practice/main.go
@@ -1,9 +1,11 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"io/ioutil"
 	"net/http"
+	"strings"
 )
 
 const url = "http://services.explorecalifornia.org/json/tours.php"
@@ -25,5 +27,38 @@ func main() {
 	}
 
 	content := string(bytes)
-	fmt.Print(content)
+	// fmt.Print(content)
+
+	tours := toursFromJson(content)
+	for _, tour := range tours {
+		fmt.Println(tour.Name)
+	}
+}
+
+func toursFromJson(content string) []Tour {
+	tours := make([]Tour, 0, 20)
+
+	// Creates a link between Json and json decoder
+	decoder := json.NewDecoder(strings.NewReader(content))
+
+	_, err := decoder.Token()
+	if err != nil {
+		panic(err)
+	}
+
+	var tour Tour
+	// While style loop (If there is more Json available, it will return true)
+	for decoder.More() {
+		err := decoder.Decode(&tour)
+		if err != nil {
+			panic(err)
+		}
+		tours = append(tours, tour)
+	}
+
+	return tours
+}
+
+type Tour struct {
+	Name, Price string
 }
